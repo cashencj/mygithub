0d91f7d93c45b90fd03649f8ed35c4fb13fe723a
Christy <cashencj@colostate.edu>
2018-11-10 16:15:37 -0700

    Anything

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Component2} from "./Component2";
+import { Button } from 'antd';
 
 class App extends Component {
+  state={
+    counter:0 
+  
+  }
+
+  handlecounter = () => this.setState({ counter: this.state.counter + 1})
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  Caribou Coffee for sale <ul>{this.state.counter} </ul>
+  <button onClick = {this.handlecounter}>click</button>
+  <Component2 number = {this.state.counter}/>
+  <Button onClick = {this.handlecounter}>Default</Button>
       </div>
+      
     );
   }
 }
 
 export default App;
+
---
+++ src/Component2.js
@@ -0,0 +1,9 @@
+import React from "react"
+export class Component2 extends React.Component{
+ render(){
+     return( <ul> colors
+        {this.props.number} </ul>
+        ) 
+     
+ }
+}
